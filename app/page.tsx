13578aa591731952f63cdd8726595bde8c69178c
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2025-05-24 10:39:45 -0600

    companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
-import Navbar from "@/components/Navbar";
-import { Button } from "@/components/ui/button";
 import React from "react";
+
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name='Neura the Brainy Explorer'
+          topic='Neural Network of the Brain'
+          subject='Science'
+          duration={45}
+          color='#E4D0FF'
+        />
+        <CompanionCard
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives & Integrals'
+          subject='Maths'
+          duration={30}
+          color='#FFDA6E'
+        />
+        <CompanionCard
+          id='789'
+          name='Verba the Vocabulary Builder'
+          topic='English Literature'
+          subject='Language'
+          duration={30}
+          color='#BDE7FF'
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
