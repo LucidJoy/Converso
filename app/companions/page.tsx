6c3b3fb6e53210be3d554ae01ed21af352aa299f
LucidJoy <jyotirmoykarmakar02@gmail.com>
2025-06-03 23:42:01 -0600

    companion library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,19 +1,45 @@
+import CompanionCard from "@/components/CompanionCard";
 import CompanionForm from "@/components/CompanionForm";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const NewCompanion = async () => {
-  const { userId } = await auth();
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
 
-  if (!userId) redirect("/sign-in");
+  const companions = await getAllCompanions({ subject, topic });
+
+  console.log(companions);
 
   return (
     <main>
-      New Companion
-      <CompanionForm />
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+
+      <section className="companions-grid">
+        {companions.map((comp) => (
+          <>
+            <CompanionCard
+              key={comp.id}
+              {...comp}
+              color={getSubjectColor(comp.subject)}
+            />
+          </>
+        ))}
+      </section>
     </main>
   );
 };
 
-export default NewCompanion;
+export default CompanionsLibrary;
--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -20,33 +20,33 @@ const CompanionCard = ({
   color,
 }: CompanionCardProps) => {
   return (
-    <article className='companion-card' style={{ backgroundColor: color }}>
-      <div className='flex justify-between items-center'>
-        <div className='subject-badge'>{subject}</div>
-        <button className='companion-bookmark'>
+    <article className="companion-card" style={{ backgroundColor: color }}>
+      <div className="flex justify-between items-center">
+        <div className="subject-badge">{subject}</div>
+        <button className="companion-bookmark">
           <Image
-            src='/icons/bookmark.svg'
-            alt='bookmark'
+            src="/icons/bookmark.svg"
+            alt="bookmark"
             width={12.5}
             height={15}
           />
         </button>
       </div>
 
-      <h2 className='text-2xl font-bold'>{name}</h2>
-      <p className='text-sm'>{topic}</p>
-      <div className='flex items-center gap-2'>
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p className="text-sm">{topic}</p>
+      <div className="flex items-center gap-2">
         <Image
-          src='/icons/clock.svg'
-          alt='duration'
+          src="/icons/clock.svg"
+          alt="duration"
           width={13.5}
           height={13.5}
         />
-        <p className='text-sm'>{duration} minutes</p>
+        <p className="text-sm">{duration} minutes</p>
       </div>
 
-      <Link href={`/companions/${id}`} className='w-full'>
-        <button className='btn-primary w-full justify-center'>
+      <Link href={`/companions/${id}`} className="w-full">
+        <button className="btn-primary w-full justify-center">
           Launch Lesson
         </button>
       </Link>
